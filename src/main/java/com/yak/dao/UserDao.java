f78667b18f9cb362ae2e3f8a036ddeb8604c820b
shawn <xcbin123@gmail.com>
2020-01-06 10:56:09 +0800

    加了示例

--- src/main/java/com/yak/dao/UserDao.java
+++ src/main/java/com/yak/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.yak.dao;
 
 import com.yak.model.User;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface UserDao {
 
     int insert(User user);
